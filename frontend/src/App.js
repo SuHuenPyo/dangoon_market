58833ead305c251bed61244f14d357b25ca2a0b0
ejbaek <909back@naver.com>
2022-02-23 16:14:07 +0900

    2022/02/23

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+
+import './css/common.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin:0,padding:0}}>
+
+       <Routes>
+         <Route path='/home' element=''/>
+         <Route path='/categori' element=''/>
+         <Route path='/cavelife' element=''/>
+         <Route path='/mygralic' element=''/>
+         <Route path='/home' element=''/>
+         <Route path='/home' element=''/>
+         <Route path='/home' element=''/>
+       </Routes>
     </div>
   );
 }
