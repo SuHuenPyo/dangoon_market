1122c4af54d4b7bdd57c1dfc54868544ea602319
shun <vytngms@gmail.com>
2022-04-11 19:08:38 +0900

    Revert "#95 -close #95"
    This reverts commit 7888f697dedac95dc3386eef96765cc6e19c5df0.

--- backend/router/like.js
+++ backend/router/like.js
@@ -22,89 +22,51 @@ let json = null;
  *     produces:
  *     - "application/json"
  *     parameters:
- *     - name: "b_id"
+ *     - name: "page"
  *       in: "query"
- *       description: "해당 기능을 적용할 b_id를 입력하세요"
+ *       description: "미정"
  *       type: "number"
- *     - name: "l_type"
+ *     - name: "rows"
  *       in: "query"
- *       description: " C, S 둘중하나 C = 동굴생활, S = 판매게시판"
- *       type: "string"
- *     - name: "l_flag"
- *       in: "query"
- *       description: "true = 좋아요 체크,    false = 좋아요 체크 해제"
- *       type: "boolean"
+ *       description: "미정"
+ *       type: "number"
  *     responses:
  *       "200":
  *         description: "successful operation"
- *       "400":
- *         description: "query 값 검증에 실패했음."
- *       "418":
- *         description: "이미 해당 기능을 완료함 (ex > 이미 좋아요가 되어있음, 이미 해제되어있음)"
  *     
 */
 Like.get('/', authIsOwner, async(req, res, next)=>{
-
-    /**
-     * 필요항목 
-     * 좋아요 클릭할 게시물 B_ID
-     * 좋아요 체크인지, 해제인지 Flag값 
-     */
-
-    //필요항목 
-    let user_id = req.session.user.id; //세션상 user id(NickName)
-    let user_m_id = null; //DB m_id
-    let b_id = req.query.b_id;
-    let l_type = req.query.l_type;
-    let l_flag = req.query.l_flag;
-    //검증
-    if(b_id == undefined){
-        return res.status(400).json({text: '검증실패'});
-    }
 
     //DB Connection
     let dbcon = new DG_DB();
     let result = null;
     try{
         await dbcon.DbConnect();
-        //해당 기능을 수행할 해당유저의 M_id 가져오기
+
+
         //좋아요
-        [result] = await dbcon.sendQuery(`SELECT m_id FROM dangoon.member WHERE m_user_id=?`, user_id);
-        user_m_id = result[0].m_id;
+        [result] = await dbcon.sendQuery(`SELECT COUNT(*) as cnt FROM dangoon.board WHERE B_TYPE='S'`);
+        //console.log(result);
 
+        const totalCount = result[0].cnt;
+        pagenationResult = pagenation(totalCount, page, rows);
 
-        /**
-         * 1. 테이블이 존재하고 1일때 플래그를 1을 요청할 경우 => 패스          /
-         * 2. 테이블이 존재하고 1일때 플래그를 0을 요청할 경우 => 쿼리실행      /
-         * 3. 테이블이 존재하고 0일때 플래그를 1을 요청할 경우 => 쿼리실행  
-         * 4. 테이블이 존재하고 0일때 플래그를 0을 요청할 경우 => 패스          /
-         * 5. 테이블이 존재하지 않고 플래그 1을 요청할 경우 => 쿼리실행(생성)   
-         * 6. 테이블이 존재하지 않고 플래그 0을 요청할 경우 => 패스             /
-         */
+        //console.log(JSON.stringify(pagenationResult));
 
+        let args = [];
 
-        //조회해서 있다면 Update, 없다면 Insert .   단, 좋아요가 되어있는지 안되어있는지는 확인하지 않는다. 중요하지 않기때문  
-        [result] = await dbcon.sendQuery(`SELECT l_flag FROM dangoon.like WHERE (m_id=? AND b_id=? AND l_type=?)`, user_m_id, b_id, l_type);
+        //데이터 조회 
+        
+        [result] = await dbcon.sendQuery(`SELECT b_id, b_writer, b_title, b_content, date_format(b_rdate, '%Y-%m-%d %H:%i:%s')as b_rdate, b_category, b_price FROM dangoon.board WHERE b_type='S' LIMIT ?,?`, pagenationResult.offset, pagenationResult.listCount);
 
-        //이미 테이블이 존재하고 1일때
-        if(result[0] && result[0].l_flag == 1){
-            //플래그 0 요청할 경우
-            if(l_flag == 0){
-                await dbcon.sendQuery(`UPDATE dangoon.like SET l_flag = 0 WHERE (m_id=? AND b_id=? AND l_type=?)`,user_m_id, b_id, l_type);
-            }
-            //플래그 1요청할 경우 무시 
+        //console.log(result2);
+        let regexp = /\B(?=(\d{3})+(?!\d))/g;
+        //console.log(result)
 
-        }else if(result[0] && result[0].l_flag == 0){
-            if(l_flag == 1){
-                await dbcon.sendQuery(`UPDATE dangoon.like SET l_flag = 1 WHERE (m_id=? AND b_id=? AND l_type=?)`,user_m_id, b_id, l_type);
-            }
-
-        }else if(!result[0]){//여기는 테이블이 존재하지 않음 
-            if(l_flag == 1){
-                await dbcon.sendQuery(`INSERT INTO dangoon.like(m_id, b_id, l_flag, l_type) VALUES(?, ?, ?, ?)`,user_m_id, b_id, l_flag ,l_type);
-            }
-        }
-
+        result.forEach(element => {
+            element.b_price = element.b_price.toString().replace(regexp, ',');
+        });
+        
     }catch(e){
         return next(e);
     }finally{
@@ -112,6 +74,6 @@ Like.get('/', authIsOwner, async(req, res, next)=>{
         await dbcon.end();
     }
     //저장한 값 여기서 전송해주고 
-    return res.status(200).json({text: '정상처리 완료'});
+    res.send({'item': result, 'pageEnd': pagenationResult.totalPage});
 });
 export default Like;
