d851b86a07ead7332fbbce7473b6408623670b77
shun <vytngms@gmail.com>
2022-04-21 17:26:30 +0900

    -close #109

--- backend/router/profile.js
+++ backend/router/profile.js
@@ -14,11 +14,14 @@ const Profile = express.Router();
  * @swagger
  * /profile:
  *   get:
- *     description: 단군마켓 유저 프로필을 조회합니다. 판매횟수, 구매횟수, description, 프로필 이미지가 리턴됩니다. , 
+ *     description: 단군마켓 유저 프로필을 조회합니다. 유저이름, sellbuyCnt(판매,구매횟수 순서대로), description, 프로필 이미지가 리턴됩니다. (b_id에 해당하는 글쓴이 정보), 
  *     tags: [Get (Working)]
  *     produces:
  *     - "application/json"
  *     parameters:
+ *     - name: "b_id"
+ *       in: "query"
+ *       description: "해당 b_id"
  *     responses:
  *       "200":
  *         description: "successful operation"
@@ -37,6 +40,7 @@ Profile.get('/', authIsOwner, async(req, res, next)=>{
      */
     let user_id = req.session.user.id; //세션상 user id(NickName)
     let user_m_id = null; //DB m_id
+    let b_id = req.query.b_id
 
     //return result value
     let return_info = {};
@@ -47,17 +51,23 @@ Profile.get('/', authIsOwner, async(req, res, next)=>{
     try{
         await dbcon.DbConnect();
 
-        //해당 접속자 세션정보로 m_id를 가져온다 
-        [result] = await dbcon.sendQuery(`SELECT m_id FROM dangoon.member WHERE m_user_id=?`, user_id);
+        //해당 b_id의 소유자를 가져온다. 
+        [result] = await dbcon.sendQuery(`SELECT m_id FROM dangoon.board WHERE b_id=?`, b_id);
         user_m_id = result[0].m_id;
 
-        console.log(user_m_id);
-
-        //전체 데이터 수 조회
+        //소유자의 이름과, 사진을 가져온다. 
         [result] = await dbcon.sendQuery(`SELECT DISTINCT m_user_id, m_name, m_email, m_pic, m_kakao_id FROM dangoon.member WHERE m_id=?`, user_m_id);
-        return_info = {'m_user_id': result[0].m_user_id, 'm_name': result[0].m_name, 'm_email': result[0].m_email, 'm_pic':S3URL+result[0].m_pic, 'm_kakao_id': result[0].m_kakao_id }
+        return_info.m_name = JSON.parse(JSON.stringify(result[0].m_name));
+        return_info.m_pic  = JSON.parse(JSON.stringify(S3URL+result[0].m_pic));
 
+        //소유자의 판매내역(판매중인것, 판매완료한것), 구매내역(완료된 것만) 을 가져온다.
+        [result] = await dbcon.sendQuery(`SELECT COUNT(*) AS cnt FROM dangoon.board as db WHERE (m_id=? AND b_type='S') UNION SELECT COUNT(*) FROM dangoon.request_purchased WHERE (m_id=? AND r_done=1)`, user_m_id, user_m_id);        
 
+        return_info.sellBuyCnt = [];
+        for(let idx in result){
+            return_info.sellBuyCnt.push(JSON.parse(JSON.stringify(result[idx].cnt)));
+        }
+        
         
     }catch(e){
         return next(e);
